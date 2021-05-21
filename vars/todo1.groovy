76baccccad63d3839fbef8ed9a63941d9aa22173
swathi-ops <swathipottabathini@gmail.com>
2021-05-21 15:08:01 +0530

    Jenkinscript

--- vars/todo1.groovy
+++ vars/todo1.groovy
@@ -4,17 +4,17 @@ def make_artifacts(APP_TYPE, COMPONENT) {
         def execute_com = sh(returnStdout: true, script: command)
         print execute_com
     }
-    elseif(APP_TYPE == "GO_LANG") {
+    else if(APP_TYPE == "GO_LANG") {
         command "zip -r ${COMPONENT}.zip login-ci"
         def execute_com = sh(returnStdout: true, script: command)
         print execute_com
     }
-    elseif(APP_TYPE == "JAVA") {
+    else if(APP_TYPE == "JAVA") {
         command "cp target/*.jar ${COMPONENT}.jar && zip -r ${COMPONENT}.zip ${COMPONENT}.jar"
         def execute_com = sh(returnStdout: true, script: command)
         print execute_com
     }
-    elseif(APP_TYPE == "NODEJS") {
+    else if(APP_TYPE == "NODEJS") {
         command "zip -r ${COMPONENT}.zip node_modules server.js"
         def execute_com = sh(returnStdout: true, script: command)
         print execute_com
@@ -27,17 +27,17 @@ def code_build(APP_TYPE, COMPONENT) {
         def execute_com = sh(returnStdout: true, script: command)
         print execute_com
     }
-    elseif(APP_TYPE == "GO_LANG") {
+    else if(APP_TYPE == "GO_LANG") {
         command "go get github.com/dgrijalva/jwt-go && go get github.com/labstack && go get github.com/labstack/echo/middleware && go get github.com/labstack/gommon/log && go get github.com/openzipkin/zipkin-go && go get github.com/openzipkin/zipkin-go/middleware/http && go get github.com/openzipkin/zipkin-go/reporter/http && go build"
         def execute_com = sh(returnStdout: true, script: command)
         print execute_com
     }
-    elseif(APP_TYPE == "JAVA") {
+    else if(APP_TYPE == "JAVA") {
         command "mvn clean package"
         def execute_com = sh(returnStdout: true, script: command)
         print execute_com
     }
-    elseif(APP_TYPE == "NODEJS") {
+    else if(APP_TYPE == "NODEJS") {
         command "npm install"
         def execute_com = sh(returnStdout: true, script: command)
         print execute_com
