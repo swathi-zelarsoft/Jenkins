a1f1e765d8691f2feddc60e4d1bdb91d56258278
swathi-ops <swathipottabathini@gmail.com>
2021-05-26 11:12:05 +0530

    Jenkinscript

--- seed.groovy
+++ seed.groovy
@@ -5,37 +5,39 @@ folder('CI-Pipelines') {
 def component = ["login", "todo","users","frontend"];
 def count =(component.size()-1)
 for (i in 0..count) {
-    def j=component[i]
-pipelineJob("CI-Pipelines/frontend-ci"){
-    configure { flowdefinition ->
-        flowdefinition / 'properties' << 'org.jenkinsci.plugins.workflow.job.properties.PipelineTriggersJobProperty' {
-        'triggers' {
-            'hudson.triggers.SCMTrigger' {
-                'spec'('*/2 * * * 1-5')
-                'ignorePostCommitHooks'(false)
-            }
-        }
-    }
-
-        flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
-            'scm'(class: 'hudson.plugins.git.GitSCM', plugin: 'git') {
-                'userRemoteConfigs' {
-                    'hudson.plugins.git.UserRemoteConfig' {
-                        'url'('https://github.com/swathi-zelarsoft/'+j+'.git')
-                        'refspec'('\'+refs/tags/*\':\'refs/remotes/origin/tags/*\'')
-
-                    } }
-                'branches' {
-                    'hudson.plugins.git.BranchSpec' {
-                        'name'('*/tags/*')
+    def j = component[i]
+    pipelineJob("CI-Pipelines/frontend-ci") {
+        configure { flowdefinition ->
+            flowdefinition / 'properties' << 'org.jenkinsci.plugins.workflow.job.properties.PipelineTriggersJobProperty' {
+                'triggers' {
+                    'hudson.triggers.SCMTrigger' {
+                        'spec'('*/2 * * * 1-5')
+                        'ignorePostCommitHooks'(false)
                     }
                 }
             }
 
+            flowdefinition << delegate.'definition'(class: 'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition', plugin: 'workflow-cps') {
+                'scm'(class: 'hudson.plugins.git.GitSCM', plugin: 'git') {
+                    'userRemoteConfigs' {
+                        'hudson.plugins.git.UserRemoteConfig' {
+                            'url'('https://github.com/swathi-zelarsoft/' + j + '.git')
+                            'refspec'('\'+refs/tags/*\':\'refs/remotes/origin/tags/*\'')
 
-            'scriptPath'('Jenkinsfile')
-            'lightweight'(true)
+                        }
+                    }
+                    'branches' {
+                        'hudson.plugins.git.BranchSpec' {
+                            'name'('*/tags/*')
+                        }
+                    }
+                }
+
+
+                'scriptPath'('Jenkinsfile')
+                'lightweight'(true)
+            }
         }
-    }
 
+    }
 }
