be18ccf06813e01ac0385b9bc396388e69436c14
swathi-ops <swathipottabathini@gmail.com>
2021-05-26 12:56:01 +0530

    Jenkinscript

--- vars/todo1.groovy
+++ vars/todo1.groovy
@@ -23,7 +23,8 @@ def make_artifacts(APP_TYPE, COMPONENT) {
 
 def code_build(APP_TYPE, COMPONENT) {
     if(APP_TYPE == "NGINX") {
-        command = "npm install && npm run build"
+       // command = "npm install && npm run build"
+        command = "env"
         def execute_com=sh(returnStdout: true, script: command)
         print execute_com
     }
