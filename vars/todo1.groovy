47d4f8c1532319cf8dea2b5ec7a8184c79d7518f
swathi-ops <swathipottabathini@gmail.com>
2021-05-27 12:02:23 +0530

    Jenkinscript

--- vars/todo1.groovy
+++ vars/todo1.groovy
@@ -51,6 +51,6 @@ def nexus(COMPONENT) {
     get_branch = "env | grep GIT_BRANCH | awk -F / '{print \$NF}' | xargs echo -n"
     def get_branch_exec=sh(returnStdout: true, script: get_branch)
     def FILENAME=COMPONENT+'-'+get_branch_exec+'.zip'
-    command = "curl -f -v -u admin:admin123 --upload-file /home/ubuntu/workspace/CI-Pipelines/${COMPONENT}-ci/${FILENAME} http://${NEXUS_IP}:8081/repository/${COMPONENT}/${FILENAME}"
+    command = "curl -f -v -u admin:admin123 --upload-file /home/ubuntu/workspace/CI-Pipelines/${COMPONENT}-ci/${FILENAME} http://192.168.0.110:8081/repository/${COMPONENT}/${FILENAME}"
     def execute_state=sh(returnStdout: true, script: command)
 }
