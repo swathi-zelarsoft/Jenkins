c63d03da7cef97c72738d29e210376ba6eaa8457
swathi-ops <swathipottabathini@gmail.com>
2021-05-28 11:05:17 +0530

    Jenkinscript

--- vars/todo1.groovy
+++ vars/todo1.groovy
@@ -1,7 +1,7 @@
 def make_artifacts(APP_TYPE, COMPONENT) {
-    get_branch = "env | grep GIT_BRANCH |awk -F / '{print \$NF}' | xargs echo -n"
-    def get_branch_exec=sh(returnStdout: true, script: get_branch)
-    def FILENAME=COMPONENT+'-'+get_branch_exec+'.zip'
+    got_branch = "env | grep GIT_BRANCH |awk -F / '{print \$NF}' | xargs echo -n"
+    def got_branch_exec=sh(returnStdout: true, script: got_branch)
+    def FILENAME=COMPONENT+'-'+got_branch_exec+'.zip'
     if(APP_TYPE == "NGINX") {
         command = "zip -r ${FILENAME} dist node_modules"
         def execute_com=sh(returnStdout: true, script: command)
@@ -18,26 +18,26 @@ def make_artifacts(APP_TYPE, COMPONENT) {
         print execute_com
     }
     else if(APP_TYPE == "NODEJS") {
-        command = "zip -r ${FILENAME} node_modules server.js"
-        def execute_com=sh(returnStdout: true, script: command)
+        comm = "zip -r ${FILENAME} node_modules server.js"
+        def execute_com=sh(returnStdout: true, script: comm)
         print execute_com
     }
 }
 
 def code_build(APP_TYPE, COMPONENT) {
     if(APP_TYPE == "NGINX") {
-        command = "npm install && npm run build"
-        def execute_com=sh(returnStdout: true, script: command)
+        comm = "npm install && npm run build"
+        def execute_com=sh(returnStdout: true, script: comm)
         print execute_com
     }
     else if(APP_TYPE == "GO_LANG") {
-        command = "go get github.com/dgrijalva/jwt-go && go get github.com/labstack/echo && go get github.com/labstack/echo/middleware && go get github.com/labstack/gommon/log && go get github.com/openzipkin/zipkin-go && go get github.com/openzipkin/zipkin-go/middleware/http && go get github.com/openzipkin/zipkin-go/reporter/http && go build"
-        def execute_com=sh(returnStdout: true, script: command)
+        comm = "go get github.com/dgrijalva/jwt-go && go get github.com/labstack/echo && go get github.com/labstack/echo/middleware && go get github.com/labstack/gommon/log && go get github.com/openzipkin/zipkin-go && go get github.com/openzipkin/zipkin-go/middleware/http && go get github.com/openzipkin/zipkin-go/reporter/http && go build"
+        def execute_com=sh(returnStdout: true, script: comm)
         print execute_com
     }
     else if(APP_TYPE == "JAVA") {
-        command = "mvn clean package"
-        def execute_com=sh(returnStdout: true, script: command)
+        comm = "mvn clean package"
+        def execute_com=sh(returnStdout: true, script: comm)
         print execute_com
     }
     else if(APP_TYPE == "NODEJS") {
@@ -48,9 +48,9 @@ def code_build(APP_TYPE, COMPONENT) {
 }
 
 def nexus(COMPONENT) {
-    get_branch = "env | grep GIT_BRANCH | awk -F / '{print \$NF}' | xargs echo -n"
-    def get_branch_exec=sh(returnStdout: true, script: get_branch)
-    def FILENAME=COMPONENT+'-'+get_branch_exec+'.zip'
+    got_branch = "env | grep GIT_BRANCH | awk -F / '{print \$NF}' | xargs echo -n"
+    def got_branch_exec=sh(returnStdout: true, script: got_branch)
+    def FILENAME=COMPONENT+'-'+got_branch_exec+'.zip'
     command = "curl -f -v -u admin:admin123 --upload-file /home/ubuntu/workspace/CI-Pipelines/${COMPONENT}-ci/${FILENAME} http://192.168.0.110:8081/repository/${COMPONENT}/${FILENAME}"
     def execute_state=sh(returnStdout: true, script: command)
 }
