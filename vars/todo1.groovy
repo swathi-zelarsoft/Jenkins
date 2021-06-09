98d6e713d9ee0971fe3d9bff25c8e2b25ca14455
swathi-ops <swathipottabathini@gmail.com>
2021-06-09 14:57:29 +0530

    jenkins

--- vars/todo1.groovy
+++ vars/todo1.groovy
@@ -27,7 +27,7 @@ def make_artifacts(APP_TYPE, COMPONENT) {
 
 def code_build(APP_TYPE, COMPONENT) {
     if(APP_TYPE == "NGINX") {
-        command = "npm install && npm run build"
+        command = "npm install --save-dev node-sass && npm run build"
         def execute_com=sh(returnStdout: true, script: command)
         print execute_com
     }
