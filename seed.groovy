e77aedbfc7b5ee5a1da583c044ca5eb52d8b2a60
swathi-ops <swathipottabathini@gmail.com>
2021-06-13 14:07:52 +0530

    initial

--- seed.groovy
+++ seed.groovy
@@ -41,3 +41,25 @@ for (i in 0..count) {
 
     }
 }
+    pipelineJob("Deployment-pipeline") {
+        configure { flowdefinition ->
+
+            flowdefinition << delegate.'definition'(class: 'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition', plugin: 'workflow-cps') {
+                'scm'(class: 'hudson.plugins.git.GitSCM', plugin: 'git') {
+                    'userRemoteConfigs' {
+                        'hudson.plugins.git.UserRemoteConfig' {
+                            'url'('https://github.com/swathi-zelarsoft/jenkins.git')
+                        }
+                    }
+                    'branches' {
+                        'hudson.plugins.git.BranchSpec' {
+                            'name'('*/main')
+                        }
+                    }
+                }
+                'scriptPath'('Jenkinsfile-Deployment')
+                'lightweight'(true)
+            }
+        }
+
+    }
