eb299a78e35bba3612b89a409fa82b299e8684f2
swathi-ops <swathipottabathini@gmail.com>
2021-06-25 12:11:52 +0530

    deployment

--- vars/todo2.groovy
+++ vars/todo2.groovy
@@ -54,7 +54,7 @@ def call(Map params = [:]) {
                         get_branch = "env | grep GIT_BRANCH | awk -F / '{print \$NF}' | xargs echo -n"
                         env.get_branch_exec=sh(returnStdout: true, script: get_branch)
                     }
-                    build job: 'Deployment Pipeline', parameters: [string(name: 'ENV', value: 'dev'), string(name: 'COMPONENT', value: "${COMPONENT}"), string(name: 'VERSION', value: "${get_branch_exec}")]
+                    build job: 'Deployment-pipeline', parameters: [string(name: 'ENV', value: 'dev'), string(name: 'COMPONENT', value: "${COMPONENT}"), string(name: 'VERSION', value: "${get_branch_exec}")]
                 }
             }
 
