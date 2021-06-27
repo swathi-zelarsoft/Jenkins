e6820900fc1c96b8e819e2826684b0780903dbb9
swathi-ops <swathipottabathini@gmail.com>
2021-06-27 07:33:46 +0530

    deployment

--- vars/todo2.groovy
+++ vars/todo2.groovy
@@ -1,7 +1,7 @@
 def call(Map params = [:]) {
     // Start Default Arguments
     def args = [
-            NEXUS_IP               : '172.31.212.18',
+            NEXUS_IP               : '192.168.0.16',
     ]
     args << params
 
