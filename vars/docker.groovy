a81749a00341956439dcbb498e72c3f107ebff8a
swathi-ops <swathipottabathini@gmail.com>
2021-08-06 11:08:24 +0530

    initial

--- vars/docker.groovy
+++ vars/docker.groovy
@@ -32,8 +32,8 @@ def call(Map params = [:]) {
                         env.get_branch_exec=sh(returnStdout: true, script: get_branch)
                     }
                     sh '''
-            aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 734529938452.dkr.ecr.us-east-1.amazonaws.com
-            docker build -t 734529938452.dkr.ecr.us-east-1.amazonaws.com/${COMPONENT}:${get_branch_exec} .
+           aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 370312947008.dkr.ecr.us-east-1.amazonaws.com
+            docker build -t 370312947008.dkr.ecr.us-east-1.amazonaws.com/${COMPONENT}:${get_branch_exec} .
           '''
                 }
             }
@@ -45,7 +45,7 @@ def call(Map params = [:]) {
                         env.get_branch_exec=sh(returnStdout: true, script: get_branch)
                     }
                     sh '''
-            docker push 734529938452.dkr.ecr.us-east-1.amazonaws.com/${COMPONENT}:${get_branch_exec}
+            docker push 370312947008.dkr.ecr.us-east-1.amazonaws.com/${COMPONENT}:${get_branch_exec}
           '''
                 }
             }
